acf01e74500e3508bd9e5b49c728ba3420fe5167
Jalen Yuan <yuanzhonghua00@gmail.com>
2024-03-07 23:23:58 -0800

    vector

---
+++ include/vector.hpp
@@ -0,0 +1,122 @@
+#ifndef HL_VECTOR
+#define HL_VECTOR
+
+#include <algorithm>
+#include <iostream>
+
+template<typename T>
+class myvector {
+  public:
+    myvector() = default;
+
+    ~myvector() { delete[] __data; };
+
+    myvector(const myvector &v) {
+        __capacity = v.__capacity;
+        __size = v.__size;
+        if (__size != 0) {
+            __data = new T[__capacity];
+            for (size_t i = 0; i < __size; ++i) { __data[i] = v[i]; }
+        }
+    }
+
+    myvector &operator=(const myvector &v) {
+        if (this != &v) {
+            delete[] __data;
+            __data == nullptr;
+
+            __capacity = v.__capacity;
+            __size = v.__size;
+            if (__size != 0) {
+                __data = new T[__capacity];
+                for (size_t i = 0; i < __size; ++i) { __data[i] = v[i]; }
+            }
+        }
+        return *this;
+    }
+
+    myvector(myvector &&v) {
+        __data = v.__data;
+        __size = v.__size;
+        __capacity = v.__capacity;
+        v.__data = nullptr;
+        v.__size = 0;
+        v.__capacity = 0;
+    }
+
+    myvector &operator=(myvector &&v) {
+        if (this != &v) {
+            delete[] __data;
+            __data = v.__data;
+            __size = v.__size;
+            __capacity = v.__capacity;
+            v.__data = nullptr;
+            v.__size = 0;
+            v.__capacity = 0;      
+        }
+        return *this;
+    }
+
+
+    [[nodiscard]] size_t size() const noexcept { return __size; };
+
+    [[nodiscard]] size_t capacity() const noexcept { return __capacity; };
+
+    [[nodiscard]] bool empty() const noexcept { return __size == 0; };
+
+    const T &operator[](size_t i) const { return __data[i]; };
+
+    T &operator[](size_t i) { return __data[i]; };
+
+    void push_back(const T &value);
+
+    void push_back(T &&value);
+
+    void pop_back() { __size--; };
+
+    // for test
+    void print() const noexcept {
+        std::cout << "Here goes vecotr:" << std::endl;
+        for (size_t i = 0; i < __size; ++i) {
+            std::cout << __data[i] << " " << std::endl;
+        }
+    }
+
+
+  private:
+    T *__data{nullptr};
+    size_t __size{0};
+    size_t __capacity{0};
+
+    static constexpr size_t MINIMUM_CAPACITY = 8;
+};
+
+template<typename T>
+void myvector<T>::push_back(const T &value) {
+    if (__size == __capacity) {
+        auto __old_data = __data;
+        __capacity = std::max(MINIMUM_CAPACITY, 2 * __size);
+        __data = new T[__capacity];
+        for (size_t i = 0; i < __size; ++i) {
+            __data[i] = __old_data[i];
+        }
+        delete[] __old_data;
+    }
+    __data[__size++] = value;
+}
+
+template<typename T>
+void myvector<T>::push_back(T &&value) {
+    if (__size == __capacity) {
+        __capacity = std::max(MINIMUM_CAPACITY, 2 * __size);
+        auto __new_data = new T[__capacity];
+        for (size_t i = 0; i < __size; ++i) {
+            __new_data[i] = __data[i];
+        }
+        delete[] __data;
+        __data = __new_data;
+    }
+    __data[__size++] = std::move(value);
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include "queue.hpp"
+#include "vector.hpp"
 
 using std::cout;
 using std::endl;
@@ -8,6 +9,8 @@ using std::endl;
 void Usage(char *progname);
 
 void test_queue();
+
+void test_vector();
 
 int main(int argc, char **argv) {
     cout << "Hello World!" << endl;
@@ -17,6 +20,8 @@ int main(int argc, char **argv) {
 
     if (std::string(argv[1]) == "queue") {
         test_queue();
+    } else if (std::string(argv[1]) == "vector") {
+        test_vector();
     }
 
     return EXIT_SUCCESS;
@@ -40,4 +45,17 @@ void test_queue() {
     // and will be destroyed at the same time it normally would have benn
     // like it goes out of scope
     q.print();
+}
+
+void test_vector() {
+    myvector<char> v;
+    v.push_back('a');
+    v.push_back('b');
+    v.push_back('c');
+
+    myvector<char> new_v = v;
+    new_v.print();
+
+    myvector<char> new_new_v(v);
+    new_new_v.print();
 }
